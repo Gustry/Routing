62aa8b891d55906d20d8197d4b42ca1bc4c3ec8c
Etienne Trimaille <etienne.trimaille@gmail.com>
2015-04-23 09:13:31 +0200

    docstring

--- core/graph.py
+++ core/graph.py
@@ -39,7 +39,8 @@ class Graph:
 
     def _fill_layer(self):
         for vertex in self.get_id_vertices():
-            self.vertices_layer_dp.addFeatures([self.get_vertex_feature(vertex)])
+            self.vertices_layer_dp.addFeatures([
+                self.get_vertex_feature(vertex)])
 
         self.vertices_layer_dp.updateExtents()
 
@@ -47,28 +48,56 @@ class Graph:
             self.vertices.insertFeature(feature)
 
     def get_vertices(self):
+        """Get a generator to loop over all vertices.
+        :return: A list of vertices.
+        :rtype: list
+        """
         nb_vertices = self.graph.vertexCount()
         return (self.graph.vertex(i) for i in range(0, nb_vertices))
 
     def get_id_vertices(self):
+        """Get a generator to loop over all vertices id.
+        :return: A list of ids.
+        :rtype: list
+        """
         nb_vertices = self.graph.vertexCount()
         return xrange(0, nb_vertices)
 
     def get_arcs(self):
+        """Get a generator to loop over all arcs.
+        :return: A list of arcs.
+        :rtype: list
+        """
         nb_edges = self.graph.arcCount()
         return (self.graph.arc(i) for i in range(0, nb_edges))
 
     def get_id_arcs(self):
+        """Get a generator to loop over all arcs id.
+        :return: A list of ids.
+        :rtype: list
+        """
         nb_edges = self.graph.arcCount()
         return xrange(0, nb_edges)
 
     def get_arc_count(self):
+        """Get the number of arc.
+        :return: The number of arc.
+        :rtype: int
+        """
         return self.graph.arcCount()
 
     def get_arc(self, id_arc):
+        """Get an arc according to an id.
+        :return: The arc.
+        :rtype: QgsGraphArc
+        """
         return self.graph.arc(id_arc)
 
     def get_arc_line(self, id_arc):
+        """Get the geometry of an arc according to an id.
+        :return: The geometry.
+        :rtype: QgsGeometry
+        """
         arc = self.get_arc(id_arc)
         point_start = self.get_vertex_point(arc.inVertex())
         point_end = self.get_vertex_point(arc.outVertex())
@@ -76,30 +105,60 @@ class Graph:
         return QgsGeometry.fromPolyline(points)
 
     def get_arc_feature(self, id_arc):
+        """Get the feature of an arc according to an id.
+        :return: The feature.
+        :rtype: QgsFeature
+        """
         feature = QgsFeature()
         geom = self.get_arc_line(id_arc)
         feature.setGeometry(geom)
         return feature
 
     def get_vertex_count(self):
+        """Get the number of vertices.
+        :return: The number of vertices.
+        :rtype: int
+        """
         return self.graph.vertexCount()
 
     def get_vertex(self, id_vertex):
+        """Get a vertex according to an id.
+        :return: The vertex.
+        :rtype: QgsGraphVertex
+        """
         return self.graph.vertex(id_vertex)
 
     def get_vertex_point(self, id_vertex):
+        """Get the point of a vertex according to an id.
+        :return: The point.
+        :rtype: QgsPoint
+        """
         return self.get_vertex(id_vertex).point()
 
     def get_vertex_geom(self, id_vertex):
+        """Get the geometry of a vertex according to an id.
+        :return: The geometry.
+        :rtype: QgsGeometry
+        """
         return QgsGeometry.fromPoint(self.get_vertex_point(id_vertex))
 
     def get_vertex_feature(self, id_vertex):
+        """Get the feature of a vertex according to an id.
+        :return: The feature.
+        :rtype: QgsFeature
+        """
         feature = QgsFeature()
         geom = self.get_vertex_geom(id_vertex)
         feature.setGeometry(geom)
         return feature
 
     def get_nearest_vertex_id(self, point):
+        """Get the nearest vertex id.
+        :param point The point.
+        :type point QgsPoint or int or QgsGraphVertex
+        :return: The closest vertex id.
+        :rtype: int
+        """
         if isinstance(point, int):
             vertex_id = point
 
@@ -117,6 +176,12 @@ class Graph:
         return vertex_id
 
     def get_nearest_vertex(self, point):
+        """Get the nearest vertex.
+        :param point The point.
+        :type point QgsPoint
+        :return The vertex.
+        :rtype QgsGraphVertex
+        """
         min = -1
         closest_vertex = None
 
@@ -129,6 +194,12 @@ class Graph:
         return closest_vertex
 
     def dijkstra(self, start):
+        """Compute dijkstra from a start point.
+        :param start The start.
+        :type start QgsPoint or int or QgsGraphVertex
+        :return Dijkstra.
+        :rtype tab
+        """
         if start not in self.dijkstra_results.keys():
             vertex_id = self.get_nearest_vertex_id(start)
             results = QgsGraphAnalyzer.dijkstra(self.graph, vertex_id, 0)
@@ -136,15 +207,25 @@ class Graph:
         return self.dijkstra_results[start]
 
     def cost_between(self, start, end):
+        """Compute cost between two points.
+        :type start QgsPoint or int or QgsGraphVertex
+        :type end QgsPoint or int or QgsGraphVertex
+        :return The cost.
+        :rtype int
+        """
         vertex_start_id = self.get_nearest_vertex_id(start)
         vertex_stop_id = self.get_nearest_vertex_id(end)
         tree, cost = self.dijkstra(vertex_start_id)
         return tree[vertex_stop_id]
 
     def show_vertices(self):
+        """DEBUG : show all vertices.
+        """
         QgsMapLayerRegistry.instance().addMapLayers([self.vertices_layer])
 
     def show_arc(self):
+        """DEBUG : show all arcs.
+        """
         layer = QgsVectorLayer("LineString", "Lines", "memory")
         dp = layer.dataProvider()
 
