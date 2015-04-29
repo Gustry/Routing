fdc990c5c83380ecf40e4d9f1744400234099e0b
Etienne Trimaille <etienne.trimaille@gmail.com>
2015-04-29 16:09:53 +0200

    refactoring graph

--- core/graph.py
+++ core/graph.py
@@ -32,43 +32,22 @@ class Graph:
         self.layer = layer
         self.director = QgsLineVectorLayerDirector(layer, -1, '', '', '', 3)
         self.director.addProperter(QgsDistanceArcProperter())
-        self.criterion = 1
+        self.num_criterion = 1
         if coef:
-            self.criterion += 1
+            self.num_criterion += 1
             self.director.addProperter(MultiplyProperter(coef, 1))
         self.crs = self.layer.crs()
         self.builder = QgsGraphBuilder(self.crs, topologyTolerance=topology_tolerance)
         self.tiedPoint = self.director.makeGraph(self.builder, points)
         self.graph = self.builder.graph()
         self.dijkstra_results = {}
-        self.vertices = QgsSpatialIndex()
-        self.vertices_layer = None
-        self.vertices_layer_dp = None
-        self._fill_layer()
 
-    def _fill_layer(self):
-        self.vertices_layer = QgsVectorLayer("Point", "Tied", "memory")
-        self.vertices_layer_dp = self.vertices_layer.dataProvider()
-
-        self.vertices_layer_dp.addAttributes([
-            QgsField("id_vertex", QVariant.Int),
-            QgsField("in_arcs_nb", QVariant.Int),
-            QgsField("out_arcs_nb", QVariant.Int),
-            QgsField("arcs_nb", QVariant.Int)
-        ])
-        self.vertices_layer.updateFields()
-
-        for vertex in self.get_id_vertices():
-            self.vertices_layer_dp.addFeatures([
-                self.get_vertex_feature(vertex)])
-
-        self.vertices_layer.updateExtents()
-
-        for feature in self.vertices_layer.getFeatures():
-            self.vertices.insertFeature(feature)
-
+    '''
+    ITERATOR
+    '''
     def get_vertices(self):
         """Get a generator to loop over all vertices.
+
         :return: A list of vertices.
         :rtype: list
         """
@@ -77,6 +56,7 @@ class Graph:
 
     def get_id_vertices(self):
         """Get a generator to loop over all vertices id.
+
         :return: A list of ids.
         :rtype: list
         """
@@ -85,6 +65,7 @@ class Graph:
 
     def get_arcs(self):
         """Get a generator to loop over all arcs.
+
         :return: A list of arcs.
         :rtype: list
         """
@@ -93,14 +74,19 @@ class Graph:
 
     def get_id_arcs(self):
         """Get a generator to loop over all arcs id.
+
         :return: A list of ids.
         :rtype: list
         """
         nb_edges = self.graph.arcCount()
         return xrange(0, nb_edges)
 
+    '''
+    ARC
+    '''
     def arc_count(self):
         """Get the number of arc.
+
         :return: The number of arc.
         :rtype: int
         """
@@ -108,16 +94,21 @@ class Graph:
 
     def get_arc(self, id_arc):
         """Get an arc according to an id.
+
         :return: The arc.
         :rtype: QgsGraphArc
         """
+        if id_arc < 0 or id_arc >= self.arc_count():
+            msg = 'Arc %s doesn\'t exist' % id_arc
+            raise GeoAlgorithmExecutionException(msg)
+
         return self.graph.arc(id_arc)
 
     def get_in_vertex_id(self, id_arc):
-        return self.graph.arc(id_arc).inVertex()
+        return self.get_arc(id_arc).inVertex()
 
     def get_out_vertex_id(self, id_arc):
-        return self.graph.arc(id_arc).outVertex()
+        return self.get_arc(id_arc).outVertex()
 
     def get_arc_linestring(self, id_arc):
         arc = self.get_arc(id_arc)
@@ -126,36 +117,12 @@ class Graph:
         linestring = [point_start, point_end]
         return linestring
 
-    def get_arc_geom(self, id_arc):
-        """Get the geometry of an arc according to an id.
-        :return: The geometry.
-        :rtype: QgsGeometry
-        """
-        linestring = self.get_arc_linestring(id_arc)
-        return QgsGeometry.fromPolyline(linestring)
-
-    def get_arc_properties(self, id_arc):
-        arc = self.get_arc(id_arc)
-        return arc.properties()
-
-    def get_arc_feature(self, id_arc):
-        """Get the feature of an arc according to an id.
-        :return: The feature.
-        :rtype: QgsFeature
-        """
-        feature = QgsFeature()
-        geom = self.get_arc_geom(id_arc)
-        out_vertex_id = self.graph.arc(id_arc).outVertex()
-        in_vertex_id = self.graph.arc(id_arc).inVertex()
-        attrs = [id_arc] + self.get_arc_properties(id_arc)
-        attrs.append(in_vertex_id)
-        attrs.append(out_vertex_id)
-        feature.setAttributes(attrs)
-        feature.setGeometry(geom)
-        return feature
-
+    '''
+    VERTEX
+    '''
     def vertex_count(self):
         """Get the number of vertices.
+
         :return: The number of vertices.
         :rtype: int
         """
@@ -163,52 +130,38 @@ class Graph:
 
     def get_vertex(self, id_vertex):
         """Get a vertex according to an id.
+
         :return: The vertex.
         :rtype: QgsGraphVertex
         """
+        if id_vertex < 0 or id_vertex >= self.vertex_count():
+            msg = 'Vertex %s doesn\'t exist' % id_vertex
+            raise GeoAlgorithmExecutionException(msg)
+
         return self.graph.vertex(id_vertex)
 
     def get_vertex_point(self, id_vertex):
         """Get the point of a vertex according to an id.
+
         :return: The point.
         :rtype: QgsPoint
         """
         return self.get_vertex(id_vertex).point()
 
-    def get_vertex_geom(self, id_vertex):
-        """Get the geometry of a vertex according to an id.
-        :return: The geometry.
-        :rtype: QgsGeometry
-        """
-        return QgsGeometry.fromPoint(self.get_vertex_point(id_vertex))
-
-    def get_vertex_feature(self, id_vertex):
-        """Get the feature of a vertex according to an id.
-        :return: The feature.
-        :rtype: QgsFeature
-        """
-        feature = QgsFeature()
-        geom = self.get_vertex_geom(id_vertex)
-        vertex = self.get_vertex(id_vertex)
-        in_arcs_id = vertex.inArc()
-        out_arcs_id = vertex.outArc()
-        attrs = [
-            id_vertex,
-            len(in_arcs_id),
-            len(out_arcs_id),
-            len(in_arcs_id) + len(out_arcs_id)]
-        feature.setAttributes(attrs)
-        feature.setGeometry(geom)
-        return feature
-
+    '''
+    SEARCHING VERTEX
+    '''
     def get_nearest_vertex_id(self, point):
         """Get the nearest vertex id.
+
         :param point The point.
-        :type point QgsPoint or int or QgsGraphVertex
+        :type point QgsPoint or int or QgsGraphVertex.
+
         :return: The closest vertex id.
         :rtype: int
         """
         if isinstance(point, int):
+            self.get_vertex(point)
             vertex_id = point
 
         elif isinstance(point, QgsGraphVertex):
@@ -226,8 +179,10 @@ class Graph:
 
     def get_nearest_vertex(self, point):
         """Get the nearest vertex.
+
         :param point The point.
         :type point QgsPoint
+
         :return The vertex.
         :rtype QgsGraphVertex
         """
@@ -242,27 +197,40 @@ class Graph:
 
         return closest_vertex
 
+    '''
+    ROUTING
+    '''
     def dijkstra(self, start, criterion=0):
         """Compute dijkstra from a start point.
+
         :param start The start.
-        :type start QgsPoint or int or QgsGraphVertex
-        :return Dijkstra.
-        :rtype tab
+        :type start QgsPoint or int or QgsGraphVertex.
+
+        :return Dijkstra : tree, cost
+        :rtype: tab
         """
 
-        if criterion > self.criterion:
-            raise GeoAlgorithmExecutionException('Property doesn\'t exist')
+        if criterion >= self.num_criterion:
+            msg = 'Property %s doesn\'t exist' % criterion
+            raise GeoAlgorithmExecutionException(msg)
 
         if start not in self.dijkstra_results.keys():
+            self.dijkstra_results[start] = {}
+
+        if criterion not in self.dijkstra_results[start].keys():
             vertex_id = self.get_nearest_vertex_id(start)
-            results = QgsGraphAnalyzer.dijkstra(self.graph, vertex_id, criterion)
-            self.dijkstra_results[start] = results
-        return self.dijkstra_results[start]
+            dijkstra = QgsGraphAnalyzer.dijkstra(
+                self.graph, vertex_id, criterion)
+            self.dijkstra_results[start][criterion] = dijkstra
+
+        return self.dijkstra_results[start][criterion]
 
     def cost_between(self, start, end, criterion=0):
         """Compute cost between two points.
+
         :type start QgsPoint or int or QgsGraphVertex
         :type end QgsPoint or int or QgsGraphVertex
+        
         :return The cost.
         :rtype int
         """
@@ -271,30 +239,7 @@ class Graph:
         tree, cost = self.dijkstra(vertex_start_id, criterion)
         return cost[vertex_stop_id]
 
-    def route_between(self, start, end, criterion=0):
-        cost = self.cost_between(start, end, criterion)
-        if cost < 0:
-            raise GeoAlgorithmExecutionException("Path not found")
-
-        route_layer = QgsVectorLayer("LineString", "Route %s" % cost, "memory")
-        data_provider = route_layer.dataProvider()
-
-        tree, cost = self.dijkstra(start, criterion)
-        vertex_start_id = self.get_nearest_vertex_id(start)
-        vertex_stop_id = self.get_nearest_vertex_id(end)
-        current_vertex = vertex_stop_id
-
-        while current_vertex != vertex_start_id:
-            arc_id = tree[current_vertex]
-            feature = self.get_arc_feature(arc_id)
-            data_provider.addFeatures([feature])
-            current_vertex = self.get_out_vertex_id(arc_id)
-
-        data_provider.updateExtents()
-        return route_layer
-
-    def route_between_multiline(self, start, end, criterion=0):
-        """Duplicate as above"""
+    def route_between_geom(self, start, end, criterion=0):
         cost = self.cost_between(start, end, criterion)
         if cost < 0:
             raise GeoAlgorithmExecutionException("Path not found")
@@ -312,26 +257,80 @@ class Graph:
 
         return QgsGeometry().fromMultiPolyline(multigeom)
 
-    def route_info_between(self, start, end, criterion=0):
-        return self.cost_between(start, end, criterion), self.route_between(start, end, criterion)
+    def route_between(self, start, end, criterion=0):
+        geom = self.route_between_geom(start, end, criterion)
+        return geom, geom.length(), self.cost_between(start, end, criterion)
 
     def show_route_between(self, start, end, criterion=0):
-        layer = self.route_between(start, end, criterion)
-        QgsMapLayerRegistry.instance().addMapLayers([layer])
+        route = self.route_between(start, end, criterion)
+        geom = route[0]
+        length = route[1]
+        cost = route[2]
 
+        route_layer = QgsVectorLayer("LineString", "Route %s" % cost, "memory")
+        data_provider = route_layer.dataProvider()
+
+        data_provider.addAttributes([
+            QgsField("length", QVariant.Double),
+            QgsField("cost", QVariant.Double)
+        ])
+        route_layer.updateFields()
+
+        feature = QgsFeature()
+        feature.setGeometry(geom)
+        feature.setAttributes([length, cost])
+        data_provider.addFeatures([feature])
+        data_provider.updateExtents()
+        QgsMapLayerRegistry.instance().addMapLayers([route_layer])
+
+    def isochrone(self, start, cost, criterion=0):
+        """Compute isochrone"""
+        pass
+
+    '''
+    DEBUG
+    '''
     def show_vertices(self):
         """DEBUG : show all vertices.
         """
-        QgsMapLayerRegistry.instance().addMapLayers([self.vertices_layer])
+        layer = QgsVectorLayer("Point", "Debug point", "memory")
+        layer_dp = layer.dataProvider()
 
-    def show_arc(self):
+        layer_dp.addAttributes([
+            QgsField("id_vertex", QVariant.Int),
+            QgsField("in_arcs_nb", QVariant.Int),
+            QgsField("out_arcs_nb", QVariant.Int),
+            QgsField("arcs_nb", QVariant.Int)
+        ])
+        layer.updateFields()
+
+        for id_vertex in self.get_id_vertices():
+            vertex = self.get_vertex(id_vertex)
+
+            feature = QgsFeature()
+            geom = QgsGeometry.fromPoint(self.get_vertex_point(id_vertex))
+            in_arcs_id = vertex.inArc()
+            out_arcs_id = vertex.outArc()
+            attrs = [
+                id_vertex,
+                len(in_arcs_id),
+                len(out_arcs_id),
+                len(in_arcs_id) + len(out_arcs_id)]
+            feature.setAttributes(attrs)
+            feature.setGeometry(geom)
+            layer_dp.addFeatures([feature])
+        layer.updateExtents()
+
+        QgsMapLayerRegistry.instance().addMapLayers([layer])
+
+    def show_arcs(self):
         """DEBUG : show all arcs.
         """
-        layer = QgsVectorLayer("LineString", "Lines", "memory")
+        layer = QgsVectorLayer("LineString", "Debug edges", "memory")
         dp = layer.dataProvider()
         attrs = []
         attrs.append(QgsField("id_arc", QVariant.Int))
-        for i in range(0,self.criterion):
+        for i in range(0, self.num_criterion):
             attrs.append(QgsField("cost_%s" % i, QVariant.Double))
         attrs.append(QgsField("in_vertex", QVariant.Int))
         attrs.append(QgsField("out_vertex", QVariant.Int))
@@ -339,12 +338,30 @@ class Graph:
         dp.addAttributes(attrs)
         layer.updateFields()
 
-        for edge_id in self.get_id_arcs():
-            dp.addFeatures([self.get_arc_feature(edge_id)])
+        for arc_id in self.get_id_arcs():
+            geom = QgsGeometry.fromPolyline(self.get_arc_linestring(arc_id))
+            arc = self.get_arc(arc_id)
+            out_vertex_id = self.get_out_vertex_id(arc_id)
+            in_vertex_id = self.get_in_vertex_id(arc_id)
+
+            attrs = []
+            attrs.append(arc_id)
+            attrs = attrs + arc.properties()
+            attrs.append(in_vertex_id)
+            attrs.append(out_vertex_id)
+
+            feature = QgsFeature()
+            feature.setAttributes(attrs)
+            feature.setGeometry(geom)
+
+            dp.addFeatures([feature])
 
         layer.updateExtents()
         QgsMapLayerRegistry.instance().addMapLayers([layer])
 
+    '''
+    SPECIFIC to InaSAFE
+    '''
     def cost_exits(self, idp_layer, exit_layer, criterion=0):
         idp_exit_layer = QgsVectorLayer("Point", "Exits", "memory")
         dp = idp_exit_layer.dataProvider()
